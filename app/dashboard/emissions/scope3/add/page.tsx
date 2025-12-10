4abdb5c8a3531d4fe2689b478b51cb81fb856628
shashi <iamshashiprakash@gmail.com>
2025-12-10 21:26:57 +0000

    Fix wasteType union type

--- app/dashboard/emissions/scope3/add/page.tsx
+++ app/dashboard/emissions/scope3/add/page.tsx
@@ -65,7 +65,9 @@ export default function AddScope3ActivityPage() {
   const [number2, setNumber2] = useState<string>('');
   const [number3, setNumber3] = useState<string>('');
   const [method, setMethod] = useState<string>('');
-  const [wasteType, setWasteType] = useState<string>('general_landfill');
+  const [wasteType, setWasteType] = useState<
+    'mixed_recycling' | 'general_landfill' | 'food'
+  >('general_landfill');
 
   // status
   const [loading, setLoading] = useState(false);
